57b2c7536c198bd48d16c47b27063a4ddc8b12ef
Andreas Pointner <andreas.pointner@fh-hagenberg.at>
2020-04-03 11:20:03 +0200

    #4 added trifunction

---
+++ src/test/java/at/fh/hagenberg/aist/jack/general/function/TriFunctionTest.java
@@ -0,0 +1,38 @@
+package at.fh.hagenberg.aist.jack.general.function;
+
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+import static org.testng.Assert.*;
+
+/**
+ * <p>Created by Andreas Pointner on 03.04.2020</p>
+ * <p>Test class for {@link TriFunction}</p>
+ *
+ * @author Andreas Pointner andreas.pointner@fh-hagenberg.at
+ */
+
+public class TriFunctionTest {
+
+    @Test
+    public void testApply() {
+        // given
+        // when
+        TriFunction<Integer, Integer, Integer, String> function = (a,b,c) -> String.format("%d, %d, %d", a, b, c);
+
+        // then
+        Assert.assertEquals(function.apply(1,2,3), "1, 2, 3");
+    }
+
+    @Test
+    public void testAndThen() {
+        // given
+        TriFunction<String, String, String, String> function = (a,b,c) -> String.format("%s, %s, %s", a, b, c);
+
+        // when
+        function = function.andThen(String::toUpperCase);
+
+        // then
+        Assert.assertEquals(function.apply("a", "b", "C"), "A, B, C");
+    }
+}
